d9738aa5172cceab31b0642bff30ef42db2a701c
Joanna <98634801+Bunnyleb@users.noreply.github.com>
2025-12-03 11:44:27 +0200

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -56,24 +56,24 @@ suite('Functional Tests', function () {
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
       chai
-      .request(server)
-      .keepOpen()
-      .put('/travellers')
-      .send(
-        {
+       .request(server)
+       .keepOpen()
+       .put('/travellers')
+       .send(
+         {
           "surname": "da Verrazzano"
-        })
+         })
       .end(function(err, res) {
-      assert.equal(res.status,200);
-      assert.equal(res.type, "application/json");
-      assert.equal(res.body.name, "Giovanni");
-      assert.equal(res.body.surname, "da Verrazzano");
+       assert.equal(res.status,200);
+       assert.equal(res.type, "application/json");
+       assert.equal(res.body.name, "Giovanni");
+       assert.equal(res.body.surname, "da Verrazzano");
 
-      done();
+       done();
     });
   });
 });
-
+});
 
 const Browser = require('zombie');
 
