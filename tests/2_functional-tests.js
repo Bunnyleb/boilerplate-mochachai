17707c40992fe37a748d9f56ee26e76cbe9b9b81
Joanna <98634801+Bunnyleb@users.noreply.github.com>
2025-12-03 20:19:35 +0200

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -102,7 +102,7 @@ suite('Functional Tests with Zombie.js', function () {
     // #5 – Test surname "Colombo"
     test('Submit the surname "Colombo" in the HTML form', function (done) {
        browser.fill('surname', 'Colombo');
-      browser.pressButton('submit', function(err) {
+      return browser.pressButton('submit', function(err) {
         if (err) return done(err);
     
     browser.assert.status(200);
